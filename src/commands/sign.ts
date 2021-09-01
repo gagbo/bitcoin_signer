034e8b4e11136c57cba4fae9df5ab3a4aca2a5b7
Gerry Agbobada <gerry.agbobada-ext@ledger.fr>
2021-09-01 19:35:21 +0200

    - Add test-tx in WD format - Add network - Add dummy signer   From   https://github.com/bitcoinjs/bitcoinjs-lib/issues/1697#issuecomment-844636949
    Now the only inconsistency is that the signer can't sign the
    transaction, as the randomly picked input doesn't match the randomly
    picked mnemonic/path

--- src/commands/sign.ts
+++ src/commands/sign.ts
@@ -1,6 +1,8 @@
 import type { Arguments, CommandBuilder } from 'yargs';
 import { getSeed } from '../lib/import-mnemonic';
+import { Transaction } from 'bitcoinjs-lib';
 import * as fs from 'fs';
+import * as bitcoin from 'bitcoinjs-lib';
 
 type Options = {
   path: string;
@@ -17,7 +19,24 @@ export const builder: CommandBuilder<Options, Options> = (yargs) =>
 
 export const handler = (argv: Arguments<Options>): void => {
   const { path, tx } = argv;
-  process.stdout.write(`Args are\n\tPath: ${path}\n\tTx: ${tx}`);
-  getSeed(fs.readFileSync(path, 'utf8'));
+  process.stdout.write(`Args are\n\tPath: ${path}\n\tTx: ${tx}\n`);
+  const seedStr = fs.readFileSync(path, 'utf8');
+  const seed = getSeed(seedStr);
+  const txStr = fs.readFileSync(tx, 'utf8');
+  const parsed = Transaction.fromHex(txStr);
+  process.stdout.write(`${parsed.toHex()}\n`);
+
+  const keyPair = bitcoin.ECPair.fromPrivateKey(seed.derivePath("m/49'/1'/0'/0/0").privateKey, {'network': bitcoin.networks.testnet});
+  const txb = bitcoin.TransactionBuilder.fromTransaction(parsed, bitcoin.networks.testnet);
+
+  // This assumes all inputs are spending utxos sent to the same Dogecoin P2PKH address (starts with D)
+  for (let i = 0; i < parsed.ins.length; i++) {
+    txb.sign(i, keyPair);
+  }
+
+  const signedTxHex = txb.build().toHex();
+  // Broadcast this signed raw transaction
+  console.log(signedTxHex)
+
   process.exit(0);
 };
--- src/lib/import-mnemonic.ts
+++ src/lib/import-mnemonic.ts
@@ -1,10 +1,12 @@
 import * as bip32 from 'bip32';
 import { BIP32Interface } from 'bip32';
 import * as bip39 from 'bip39';
+import { Network } from 'bitcoinjs-lib';
+import * as bitcoin from 'bitcoinjs-lib';
 
-export function getSeed(mnemonic: string): BIP32Interface {
+export function getSeed(mnemonic: string, network?: Network): BIP32Interface {
 bip39.validateMnemonic(mnemonic);
 
 const seed = bip39.mnemonicToSeedSync(mnemonic);
-return bip32.fromSeed(seed);
+return bip32.fromSeed(seed, network || bitcoin.networks.testnet);
 }
